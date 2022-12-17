0d857531daad9ede8879b0e4212611c337065cd9
emir7 <emir.ha997@gmail.com>
2022-12-17 23:55:45 +0100

    day15 part2

--- day15/grid.js
+++ day15/grid.js
@@ -7,6 +7,7 @@ module.exports = class Grid {
         this.sensors = sensors;
         this.markedSpots = new Set([]);
         this.calculatedSpots = new Set([]);
+        this.calculatedPoints = new Set([]);
 
         this.#init();
     }
@@ -22,13 +23,77 @@ module.exports = class Grid {
     }
 
     part1(y) {
-
         this.sensors.forEach((sensor) => {
             const yDiff = Math.abs(sensor.y - y);
             this.#analyse(sensor, yDiff, y);
         });
 
         console.log(this.calculatedSpots.size)
+    }
+
+    part2(searchSpace) {
+        for(let i = 0; i < this.sensors.length; i++) {
+            const sensor = this.sensors[i];
+            const invisiblePoints = this.#getInvisiblePoints(sensor, searchSpace);
+
+            for(const point of invisiblePoints) {
+                let isPointInvisible = true;
+
+                for(let j = 0; j < this.sensors.length; j++) {
+                    const otherSensor = this.sensors[j];
+    
+                    if(i == j) {
+                        continue;
+                    }
+    
+                    const distanceFromOtherSensor = Math.abs(otherSensor.x - point.x) + Math.abs(otherSensor.y - point.y);
+
+                    if(distanceFromOtherSensor <= otherSensor.visibleDistance) {
+                        isPointInvisible = false;
+                        break;
+                    }
+    
+                }
+
+                if(isPointInvisible) {
+                    console.log(point);
+                    console.log(point.x * 4000000 + point.y);
+                    return;
+                }
+            }
+   
+        }
+    }
+
+    #getInvisiblePoints(sensor, searchSpace) {
+        const points = [];
+        const farDistance = sensor.visibleDistance + 1;
+
+        for(let x = sensor.x - farDistance, d = 0; x <= sensor.x + farDistance; x++, d++) {
+            if(x < 0 || x > searchSpace) {
+                continue;
+            }
+
+
+
+            const y1 = sensor.y + d;
+            const y2 = sensor.y - d;
+
+            
+            if(y1 >= 0 && y1 < searchSpace && !this.calculatedPoints.has(`${x},${y1}`)) {
+                //this.calculatedSpots.add(`${x},${y1}`);
+                points.push({x, y: y1})
+            }
+
+
+            if(y2 >= 0 && y2 < searchSpace && !this.calculatedPoints.has(`${x},${y2}`)) {
+                //this.calculatedSpots.add(`${x},${y2}`);
+                points.push({x, y: y2})
+            }
+
+        }
+
+        return points;
     }
 
     #analyse(sensor, yDiff, y) {
@@ -41,7 +106,6 @@ module.exports = class Grid {
             }
         }
     }
-
 
     print() {
         for(let i = 0; i < this.grid.length; i++) {
--- day15/main.js
+++ day15/main.js
@@ -5,4 +5,5 @@ const parsedData = utils.parse("input.txt");
 const {maxX, maxY, minX, minY} = utils.findMinsMaxs(parsedData);
 const grid = new Grid(minX, minY, maxX, maxY, parsedData);
 
-grid.part1(2000000);
+//grid.part1(2000000);
+grid.part2(4000000)
